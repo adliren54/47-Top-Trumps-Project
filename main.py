3c91c8cbb70d0f090483d060bcadef34a1890efa
adliren54 <62419296+adliren54@users.noreply.github.com>
2023-05-03 20:36:15 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import os, time
+print("🌟Top Trumps🌟")
+print()
+print("Welcome to the Top Trumps 'Most Handsome Computing Teachers' Simulator")
+print()
+
+trumps = {}
+trumps["David"] = {"Intelligence": 178, "Speed": 4, "Guile": 80, "Baldness": 99}
+trumps["Mr Spock"] = {"Intelligence": 200, "Speed": 10, "Guile": 10, "Baldness": 10}
+trumps["Monica from Friends"] = {"Intelligence": 100, "Speed": 65, "Guile": 99, "Baldness": 0}
+trumps["Professor X"] = {"Intelligence": 160, "Speed": 0, "Guile": 5, "Baldness": 10}
+
+
+while True:
+  
